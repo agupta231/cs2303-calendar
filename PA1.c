a80d2080cad7e2607184dfcdcbc16948e1679144
Ankur Gupta <rukna1000@gmail.com>
2018-08-29 09:25:40 -0400

    Add constants for month and year

--- PA1.c
+++ PA1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define JAN 0
+#define FEB 1
+#define MAR 2
+#define APR 3
+#define MAY 4
+#define JUN 5
+#define JUL 6
+#define AUG 7
+#define SEP 8
+#define OCT 9
+#define NOV 10
+#define DEC 11
+
+#define SUN 0
+#define MON 1
+#define TUE 2
+#define WED 3
+#define THU 4
+#define FRI 5
+#define SAT 6
+
 int getDesiredYear() {
 	int userYear;
 
@@ -15,6 +36,15 @@ int getDesiredYear() {
 	return userYear;
 }
 
+int determineFirstDay(int year) {
+
+
+	int decade = year % 100;	
+
+	return 0;
+}
+
 int main(void) {
 	int year = getDesiredYear();
+	determineFirstDay(year);
 }
